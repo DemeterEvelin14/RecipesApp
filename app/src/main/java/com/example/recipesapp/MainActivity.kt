a4ad1ddb544fffb20cdcea878e75a0ff51fbec85
DemeterEvelin14 <99736246+DemeterEvelin14@users.noreply.github.com>
2023-10-30 15:52:08 +0200

    labor4

--- app/src/main/java/com/example/recipesapp/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.recipesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val TAG: String? = MainActivity::class.java.canonicalName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d(TAG, "onCreate: MainActivity created.")
+
+        val extra = intent.getStringExtra("message")
+        binding.welcomeText.text = extra
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: MainActivity started.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: MainActivity resumed.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: MainActivity paused.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: MainActivity stopped.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: MainActivity destroyed.")
     }
 }
